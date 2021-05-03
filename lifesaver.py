24c314256cb93b85ea7d7bfcc778ec29fbc28ac1
Benjamin Arntzen <zorlin@gmail.com>
2021-05-03 10:59:25 +0800

    Comment out csv stuff

--- lifesaver.py
+++ lifesaver.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # Script to throw a lifesaver at Riff! :)
-import csv
-with open('ring.csv') as file:
-    reader = csv.reader(file, delimiter=',')
-    for row in reader:
-        print(row)
+
+# Thanks to unknown20 on Python Discord for the help with the csv stuff.
+# We didn't end up using it, but it's appreciated.
+#import csv
+#with open('ring.csv','r',encoding = 'cp850') as file:
+#    reader = csv.reader(file, delimiter=',')
+#    for row in reader:
+#        print(row)
