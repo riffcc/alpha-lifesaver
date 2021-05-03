723f9a7611e25c3f8b39d66ff313cf76918588fb
Benjamin Arntzen <zorlin@gmail.com>
2021-05-03 12:04:34 +0800

    Checking this in, does not work yet

--- lifesaver.py
+++ lifesaver.py
@@ -1,10 +1,39 @@
-#!/usr/bin/python3
-# Script to throw a lifesaver at Riff! :)
+#!/opt/homebrew/bin/python3
+# Uses homebrew py3 because I am a bad person
 
-# Thanks to unknown20 on Python Discord for the help with the csv stuff.
-# We didn't end up using it, but it's appreciated.
-#import csv
-#with open('ring.csv','r',encoding = 'cp850') as file:
-#    reader = csv.reader(file, delimiter=',')
-#    for row in reader:
-#        print(row)
+# Script to throw a lifesaver at Riff! :)
+import mysql.connector
+import os
+import yaml
+
+# Dynamically load in our magic config files
+configname = os.path.expanduser('~/.rcc-tools.yml')
+config = yaml.safe_load(open(configname))
+
+# Check if the config is empty
+if config is None:
+    print("Failed to load configuration.")
+    sys.exit(1338)
+
+# Set our mysql password
+password = config["password"]
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="unit3d",
+  password=password,
+  database="unit3d"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM torrents")
+
+myresult = mycursor.fetchall()
+
+row = dict(zip(mycursor.column_names, mycursor.fetchone()))
+print("{description}".format(row))
+
+cursor = cnx.cursor()
+cursor.execute(...)
+row = dict(zip(cursor.column_names, cursor.fetchone()))
