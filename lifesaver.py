592af5b60a25fdd9098d504ec00ffc6ab9352ade
Benjamin Arntzen <zorlin@gmail.com>
2021-05-03 10:06:03 +0800

    First test

--- lifesaver.py
+++ lifesaver.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
+# Script to throw a lifesaver at Riff! :)
 import csv
+with open('ring.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
